fb6af8d176357d614ea1098f4f54cf90ae6980c1
Summerborn <devbhai2586@gmail.com>
2021-04-02 00:26:29 +0600

    Navbar styled a bit

--- resources/views/front/layouts/nav.blade.php
+++ resources/views/front/layouts/nav.blade.php
@@ -1,5 +1,5 @@
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<link href="{{ asset('/css/navbar.css') }}" rel="stylesheet">
+<nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top page-header">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">NyoTsong : Market of Possibilities</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
@@ -10,37 +10,46 @@
             <ul class="navbar-nav ml-auto">
             <li class="nav-item">
     <a class="nav-link" href="/" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-home" aria-hidden="true"></i>
     Home
     </a>
     </li>
 
     <li class="nav-item">
     <a class="nav-link" href="category">
+        <i class="fa fa-list-alt" aria-hidden="true"></i>
     Categories
     </a>
     </li>
     <li class="nav-item dropdown">
     <li class="nav-item dropdown">
     <a class="nav-link" href="about" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-info-circle" aria-hidden="true"></i>
     About
     </a>
     </li>
 
     <li class="nav-item">
     <a class="nav-link" href="contact">
+        <i class="fa fa-mobile" aria-hidden="true"></i>
     Contact
     </a>
     </li>
     <li class="nav-item dropdown active">
     <a class="nav-link" href="{{ url('/front/postadd/postad') }}" aria-haspopup="true" aria-expanded="false">
+    <button class="btn btn-light" type="button"><i class="fa fa-location-arrow" aria-hidden="true"></i>
     Post an Ad
+    </button>
     </a>
     </li>
+    
 
                 <li class="nav-item dropdown">
-                    <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#" id="bd-versions"
+                    <a class="nav-item nav-link dropdown mr-md-2" href="#" id="bd-versions"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa fa-user"></i> {{ auth()->check() ? auth()->user()->name : 'Account' }}
+                        <button class="btn btn-light" type="button"><i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                            {{ auth()->check() ? auth()->user()->name : 'Account' }}
+                        </button>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
                         @if (!auth()->check())
