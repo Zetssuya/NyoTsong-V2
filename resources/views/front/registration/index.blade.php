2059afd3c55d2ad8b350346f56494325516df161
Summerborn <devbhai2586@gmail.com>
2021-04-01 21:57:56 +0600

    Registration page is styled once more

--- resources/views/front/registration/index.blade.php
+++ resources/views/front/registration/index.blade.php
@@ -1,63 +1,80 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<link href="{{ asset('/css/register.css') }}" rel="stylesheet">
 @extends('front.layouts.master')
 
 @section('content')
 
-    <div class="row">
 
-    <div class="col-md-12" id="register">
+    <div class="container position-absolute top-50 start-50 translate-middle">
 
-        <div class="card col-md-8">
-            <div class="card-body">
-                <h2 class="card-title">Sign Up</h2>
-                <hr>
+    <div class="myCard" id="register">
 
-                @if ( $errors->any() )
+        <div class="row">
+            <div class="col-md-6">
+                <div class="myLeftCtn">
+                    <header class="text-center">Sign Up</header>
+                    <hr>
 
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    @if ( $errors->any() )
 
-                @endif
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
 
-                <form action="/user/register" method="post">
+                    @endif
 
-                    @csrf
+                    <form class="myForm col-md-6" action="/user/register" method="post">
 
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text" name="name" placeholder="Name" id="name" class="form-control">
-                    </div>
+                        @csrf
 
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="text" name="email" placeholder="Email" id="email" class="form-control">
-                    </div>
+                        <div class="form-group">
+                            <input type="text" name="name" placeholder="Name" id="name" class="form-control myInput">
+                        </div>
 
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" placeholder="Password" id="password" class="form-control">
-                    </div>
+                        <div class="form-group">
+                            <input type="text" name="email" placeholder="Email" id="email" class="form-control myInput">
+                        </div>
 
-                    <div class="form-group">
-                        <label for="password_confirmation">Confirm Password:</label>
-                        <input type="password" name="password_confirmation" placeholder="Confirm Password" id="password_confirmation" class="form-control">
-                    </div>
+                        <div class="form-group">
+                            <input type="password" name="password" placeholder="Password" id="password" class="form-control myInput">
+                        </div>
 
-                    <div class="form-group">
-                        <button class="btn btn-outline-info col-md-2"> Sign Up</button>
-                    </div>
+                        <div class="form-group">
+                            <input type="password" name="password_confirmation" placeholder="Confirm Password" id="password_confirmation" class="form-control myInput">
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                <input id="check_1" name="check_1"  type="checkbox" required><small> I read and agree to T&Cs</small> 
+                                <div class="invalid-feedback">You must check the box.</div>
+                            </label>
+                        </div>
+                            <input type="submit" class="butt" value="Create Account">
 
-                </form>
-
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="myRightCtn">
+                        <div class="box"><header>Please Note!</header>
+                        
+                        <h6>Before creating account with NyoTsong, please make sure you have read our privacy & general usage T&Cs.</h6>
+                            <input type="button" class="butt_out" value="Read Here"/>
+                        </div>
+                            
+                </div>
             </div>
         </div>
 
     </div>
-
 </div>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 @endsection
+
