2ea7ac8866dcd7a1c542ef78086a5bc575db2934
Summerborn <devbhai2586@gmail.com>
2021-04-03 01:37:26 +0600

    admin login styled

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,13 +8,17 @@
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
-
-
+     {{-- New --}}
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+    {{-- end of New --}}
+{{-- 
     <link href="{{ url('assets/css/bootstrap.min.css') }}" rel="stylesheet"/>
 
     <link href="{{ url('assets/css/animate.min.css') }}" rel="stylesheet"/>
 
-    <link href="{{ url('assets/css/paper-dashboard.css') }}" rel="stylesheet"/>
+    <link href="{{ url('assets/css/paper-dashboard.css') }}" rel="stylesheet"/> --}}
 
     <link href="{{ url('http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css') }}" rel="stylesheet">
 
@@ -22,65 +26,57 @@
 
     <link href="{{ url('assets/css/themify-icons.css') }}" rel="stylesheet">
 
+    <link href="{{ url('/css/adminLogin.css') }}" rel="stylesheet">
+    
+
 </head>
 <body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="/">
-                NyoTsong: Market of Possibilities 
-            </a>
-        </div>
-    </div>
+{{-- Admin Log in Navbar     --}}
+<nav class="navbar navbar-default navbar-dark bg-dark">
+    <a class="navbar-brand" href="{{ url('/') }}">NyoTsong : Market of Possibilities</a>
 </nav>
-<div class="wrapper">
-    <div class="container" style="margin-top: 50px">
+{{-- End of Admin Log in Navbar     --}}
+
+{{-- Admin Log in Form --}}
+<div class="container position-absolute top-50 start-50 translate-middle">
+    <div class="myCard">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title"><strong>Admin Login</strong></h3>
-                    </div>
+            <div class="col-md-6">
+                <div class="myLeftCtn">
+                    <header>Admin Log in</header>
+                    <hr>
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
 
-                    <div class="panel-body">
-
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        @if ( session()->has('msg') )
-                            <div class="alert alert-success">{{ session()->get('msg') }}</div>
-                        @endif
+                            @if ( session()->has('msg') )
+                                <div class="alert alert-success">{{ session()->get('msg') }}</div>
+                            @endif
 
 
-                        <form method="post" action="/admin/login">
+                            <form method="post" action="/admin/login" class="myForm col-md-6">
+                                @csrf
 
-                            @csrf
+                                <div class="form-group">
+                                    <input type="email" name="email" id="email" placeholder="Email"
+                                        class="form-control myInput">
+                                </div>
 
-                            <div class="form-group">
-                                <label for="email">Email:</label>
-                                <input type="email" name="email" id="email" placeholder="Email"
-                                       class="form-control border-input">
-                            </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" placeholder="Password"
+                                        class="form-control myInput">
+                                </div>
 
-                            <div class="form-group">
-                                <label for="password">Password:</label>
-                                <input type="password" name="password" id="password" placeholder="Password"
-                                       class="form-control border-input">
-                            </div>
+                                <input type="submit" class="butt myInput" value="Log in">
 
-                            <div class="form-group">
-                                <button class="btn btn-primary" type="submit">Login</button>
-                            </div>
-
-                        </form>
+                            </form>
 
                     </div>
                 </div>
@@ -88,7 +84,10 @@
         </div>
     </div>
 </div>
-
+{{-- Admin Log in Form --}}
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </html>
