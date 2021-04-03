48fdfcd974241c6b97248833db1a92c00b9e5809
Summerborn <devbhai2586@gmail.com>
2021-04-03 20:58:39 +0600

    Ads landing page styled

--- resources/views/front/postadd/postad.blade.php
+++ resources/views/front/postadd/postad.blade.php
@@ -1,16 +1,46 @@
+<link href="{{ asset('/css/postad.css') }}" rel="stylesheet">
 @extends('front.layouts.master')
 
 @section('content')
 
-<div class="page-header">
-		<div class="">
-			<a href="{!! url('/front/postadd/sale') !!}" class="btn btn-primary">For Sale <i class="fa fa-plus"></i></a>
+<div>
+	{{-- SVG Image --}}
+	<style>
+		body {
+		  background-image: url('/assets/img/onlinead.svg');
+		  background-repeat: no-repeat;
+		  background-attachment: fixed;
+		  background-size: 70% 70%;
+		  background-position: right;
+		}
+		</style>
+		<br>
+	{{-- End of SVG Image --}}
+		<div>
+			<h2 class="page-title">Post an Ad</h2>
 		</div>
-        <div class="">
-			<a href="{!! url('/front/postadd/donation') !!}" class="btn btn-primary">For Donation <i class="fa fa-plus"></i></a>
-		</div>
-		<h2>Post an Ad</h2>
-       
+		<br>
+	
+	{{-- Ads posting for sale and donation	 --}}
+		<div class="myCard">
+			{{-- <div class="">
+				<a href="{!! url('/front/postadd/sale') !!}" class="btn btn-primary">I want to sell <i class="fa fa-plus"></i></a>
+			</div> --}}
+			{{-- test --}}
+			<div class="container" >
+				<a href="{!! url('/front/postadd/sale') !!}" class="button">sell</a>
+			</div>
+			{{-- test end  --}}
+
+			<div class="container" >
+				<a href="{!! url('/front/postadd/donation') !!}" class="button second">donate</a>
+			</div>
+			{{-- <br>
+			<div class="">
+				<a href="{!! url('/front/postadd/donation') !!}" class="btn btn-primary">I want to donate <i class="fa fa-plus"></i></a>
+			</div> --}}
+		</div> 
+	{{--End of  Ads posting for sale and donation	 --}}      
 </div>
 
 @endsection
--- resources/views/front/sessions/index.blade.php
+++ resources/views/front/sessions/index.blade.php
@@ -1,55 +1,70 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<link href="{{ asset('/css/userLogin.css') }}" rel="stylesheet">
 @extends('front.layouts.master')
 
 @section('content')
-    <div class="row">
+    <div class="container position-absolute top-50 start-50 translate-middle">
 
-        <div class="col-md-12" id="register">
+        <div class="myCard" id="register">
 
-            <div class="card col-md-8">
-                <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="myLeftCtn">
+                        <header class="text-center">log in</header>
+                        <hr>
 
-                    <h2 class="card-title">Login</h2>
-                    <hr>
+                        @if ( $errors->any() )
 
-                    @if ( $errors->any() )
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
 
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                        @endif
 
-                    @endif
+                        @if ( session()->has('msg') )
 
-                    @if ( session()->has('msg') )
+                            <div class="alert alert-success">{{ session()->get('msg') }}</div>
 
-                        <div class="alert alert-success">{{ session()->get('msg') }}</div>
+                        @endif
 
-                    @endif
+                        <form action="/user/login" method="post" class="myForm col-md-6" action="/user/register" method="post">
 
-                    <form action="/user/login" method="post">
+                            @csrf
 
-                        @csrf
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="text" name="email" placeholder="Email" id="email" class="form-control myInput">
+                            </div>
 
-                        <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="text" name="email" placeholder="Email" id="email" class="form-control">
-                        </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" name="password" placeholder="Password" id="password"
+                                class="form-control myInput">
+                            </div>
 
-                        <div class="form-group">
-                            <label for="password">Password:</label>
-                            <input type="password" name="password" placeholder="Password" id="password"
-                                   class="form-control">
-                        </div>
+                            <div class="form-group">
+                                <input type="submit" class="butt" value="log in">
+                            </div>
 
-                        <div class="form-group">
-                            <button class="btn btn-outline-info col-md-2"> Login</button>
-                        </div>
+                        </form>
+                    </div>
 
-                    </form>
-
+                </div>
+                <div class="col-md-6">
+                    <div class="myRightCtn">
+                            <div class="box"><header>NyoTsong</header>
+                            
+                            <h4>Market of Possibilities</h4>
+                                {{-- <input type="button" class="butt_out" value="Read Here"/> --}}
+                            </div>
+                                
+                    </div>
                 </div>
             </div>
 
@@ -57,4 +72,7 @@
         </div>
 
     </div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 @endsection
