09d7c7ee070d1f1f0449710e32aa27ba1bba7dc8
Summerborn <devbhai2586@gmail.com>
2021-04-07 01:11:27 +0600

    navbar is back to normal one

--- resources/views/front/layouts/nav.blade.php
+++ resources/views/front/layouts/nav.blade.php
@@ -7,60 +7,58 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+        <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
     <a class="nav-link" href="/" aria-haspopup="true" aria-expanded="false">
-        <i class="fa fa-home" aria-hidden="true"></i>
+        <i class="fas fa-home"></i>
     Home
     </a>
     </li>
 
-    <li class="nav-item">
+    {{-- <li class="nav-item">
     <a class="nav-link" href="category">
         <i class="fa fa-list-alt" aria-hidden="true"></i>
     Categories
     </a>
-    </li>
+    </li> --}}
     <li class="nav-item dropdown">
     <li class="nav-item dropdown">
     <a class="nav-link" href="about" aria-haspopup="true" aria-expanded="false">
-        <i class="fa fa-info-circle" aria-hidden="true"></i>
+        <i class="fas fa-info"></i>
     About
     </a>
     </li>
 
     <li class="nav-item">
     <a class="nav-link" href="contact">
-        <i class="fa fa-mobile" aria-hidden="true"></i>
+        <i class="fas fa-phone-square"></i>
     Contact
     </a>
     </li>
     <li class="nav-item dropdown active">
     <a class="nav-link" href="{{ url('/front/postadd/postad') }}" aria-haspopup="true" aria-expanded="false">
-    <button class="btn btn-light" type="button"><i class="fa fa-location-arrow" aria-hidden="true"></i>
-    Post an Ad
+    <button class="btn btn-success postad" type="button">
+    Post an Ad <i class="fas fa-paper-plane"></i>
     </button>
     </a>
     </li>
-    
-
-                <li class="nav-item dropdown">
-                    <a class="nav-item nav-link dropdown mr-md-2" href="#" id="bd-versions"
-                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <button class="btn btn-light" type="button"><i class="fa fa-user-circle-o" aria-hidden="true"></i>
+            <li class="nav-item dropdown">
+                <a class="nav-item nav-link dropdown mr-md-2" href="#" id="bd-versions"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-light" type="button"><i class="fa fa-user-circle-o" aria-hidden="true"></i>
                             {{ auth()->check() ? auth()->user()->name : 'Account' }}
-                        </button>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
-                        @if (!auth()->check())
-                            <a class="dropdown-item " href="{{  url('user/login') }}">Login</a>
-                            <a class="dropdown-item" href="{{  url('user/register') }}">Register</a>
-                        @else
-                            <a class="dropdown-item" href="{{  url('user/profile') }}"><i class="fa fa-user"></i> Profile</a>
-                            <hr>
-                            <a class="dropdown-item" href="{{  url('user/logout') }}"><i class="fa fa-lock"></i> Logout</a>
-                        @endif
-                    </div>
+                    </button>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
+                    @if (!auth()->check())
+                        <a class="dropdown-item " href="{{  url('user/login') }}">Login</a>
+                        <a class="dropdown-item" href="{{  url('user/register') }}">Register</a>
+                    @else
+                        <a class="dropdown-item" href="{{  url('user/profile') }}"><i class="fa fa-user"></i> Profile</a>
+                        <hr>
+                        <a class="dropdown-item" href="{{  url('user/logout') }}"><i class="fa fa-lock"></i> Logout</a>
+                    @endif
+                </div>
                 </li>
 
                 <!-- <li class="nav-item">
