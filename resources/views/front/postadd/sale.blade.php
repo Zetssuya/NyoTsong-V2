0455c059ce7f82d67e3590209ee53f1fdeca240f
Summerborn <devbhai2586@gmail.com>
2021-04-06 23:46:08 +0600

    Sale page fully styled

--- resources/views/front/postadd/sale.blade.php
+++ resources/views/front/postadd/sale.blade.php
@@ -1,18 +1,23 @@
 <link href="{{ asset('/css/sale.css') }}" rel="stylesheet">
 @extends('front.layouts.master')
-
 @section('content')
-
-<div class="col-sm-12 col-md-8 col-lg-9 sale">
     <style>
-        body {
-        background-image: url('/assets/img/product_sale.svg');
+    body{
+        background-image: 
+        url('/assets/img/blue_wave.svg'),
+        url('/assets/img/red_wave.svg');
+        background-position: 
+        right,
+        left;
         background-repeat: no-repeat;
         background-attachment: fixed;
-        background-size: 55% 55%;
-        background-position: right;
-        }
-    </style>
+        background-size: 
+        20%,
+        7%;
+    }
+</style>
+<div class="col-sm-12 col-md-8 col-lg-12 row justify-content-center mt-4 container">
+
 <div class="row page-content col-md-9 col-11">
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7">
 <div class="inner-box">
@@ -34,10 +39,10 @@
 
     {{-- Product Name --}}
         <div class="dashboard-wrapper">
-        <div class="pl-lg-2 col-lg-10 col-8">
-            <small class="form-text text-muted">
+        <div class="pl-lg-2 col-lg-10 col-8 mb-4">
+            <label class="form-text text-muted">
                 Product name
-            </small>
+            </label>
         <input class="form-control" name="name" placeholder="Enter product name" autocomplete="off" type="text" aria-label="Product name">
         <span class="text-danger">{{$errors->first('name')}}</span>
 
@@ -45,10 +50,10 @@
 
         {{-- Select Category --}}
         <div class="pl-lg-2 col-lg-10 col-8">
-            <small class="form-text text-muted">
+            <label class="form-text text-muted">
                 Product category
-            </small>
-        <div class="tg-select form-control">
+            </label>
+        <div class="tg-select form-control mb-4">
         <select name="categories" required>
             <option value="none">Select category</option>
                 @foreach($categories as $i => $category)
@@ -61,28 +66,28 @@
 
         {{-- Select Price --}}
         <div class="pl-lg-2 col-lg-10 col-8">
-            <small class="form-text text-muted">
+            <label class="form-text text-muted">
                 Product price
-            </small>
+            </label>
         <input class="form-control" name="price" placeholder="Enter product price" type="text">
         <span class="text-danger">{{$errors->first('price')}}</span>
         </div>
 
         {{-- Enter product Details --}}
-        <div class="pl-lg-2 col-lg-10 col-8">
-            <small class="form-text text-muted">
+        <div class="pl-lg-2 col-lg-10 col-8 mb-4">
+            <label class="form-text text-muted">
                Product description
-            </small>
+            </label>
         <textarea class="form-control description" name="detail" placeholder="Enter product description" type="text"></textarea>
         <span class="text-danger">{{$errors->first('detail')}}</span>
         </div>
 
         {{-- Select Product Image --}}
-        <div class="pl-lg-2 col-lg-10 col-8">
-            <small class="form-text text-muted">
+        <div class="pl-lg-2 col-lg-10 col-8 mb-4 ">
+            <label class="form-text text-muted ">
                 Select product image
-             </small>
-        <input id="tg-photogallery" class="tg-fileinput" type="file" name="image">
+            </label>
+        <input id="tg-photogallery" class="tg-fileinput form-control" type="file" name="image">
         </div>
         </div>
         </div>
@@ -90,17 +95,17 @@
 
     {{-- Location in next col --}}
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5">
-    <div class="inner-box">
+    <div class="inner-box mb-4">
     <div class="tg-contactdetail">
-        <h2 class="font-weight-bold mb-5">
+        <h2 class="font-weight-bold">
             Location detail
         </h2>
-        <div class="pl-lg-2 col-lg-10 col-8">
-            <small class="form-text text-muted">
+        <div class="pl-lg-2 col-lg-6 col-8">
+            <label class="form-text text-muted">
                 Product location
-             </small>
+             </label>
         <div>
-            <select name="location" class="pl-lg-2 col-lg-12 col-8" required>
+            <select name="location" class="pl-lg-3 location form-control" required>
             <option value="none">Select location</option>
             @foreach($locations as $i => $location)
                 <option value="{{$location->location}}">{{$location->location}}</option>
@@ -111,19 +116,16 @@
     </div>
     </div>
     
-            <div class="tg-checkbox">
+            <div class="tg-checkbox mb-5">
                 <div class="custom-control custom-checkbox">
                     <input type="checkbox" class="custom-control-input" id="tg-agreetermsandrules">
                     <label class="custom-control-label required" for="tg-agreetermsandrules">I agree to all NyoTsong <a href="javascript:void(0);">Product Guidelines &amp; Conditions</a></label>
                 </div>
             </div>
             <div>
-                <button class="btn-nav" type="submit">
-                    <span class="label">
-                        Post Ad
-                    </span>
-                </button>
+                <button type="submit" class="btn btn-success    btn-lg" data-mdb-ripple-color="#d8caca" style="background-color:#169235"> Post Ad</button>
             </div>
+           
     </form>
     </div>
     {{-- End of second column --}}
