eeed5e77820a2162dbe489549e43dcefe4234eb0
Summerborn <devbhai2586@gmail.com>
2021-04-09 01:31:43 +0600

    navbar icons removed

--- resources/views/front/layouts/nav.blade.php
+++ resources/views/front/layouts/nav.blade.php
@@ -10,7 +10,6 @@
         <ul class="navbar-nav ml-auto">
     <li class="nav-item">
     <a class="nav-link" href="/" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-home"></i>
     Home
     </a>
     </li>
@@ -18,14 +17,12 @@
     <li class="nav-item dropdown">
     <li class="nav-item dropdown">
     <a class="nav-link" href="about" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-info"></i>
     About
     </a>
     </li>
 
     <li class="nav-item">
     <a class="nav-link" href="contact">
-        <i class="fas fa-phone-square"></i>
     Contact
     </a>
     </li>
@@ -42,9 +39,8 @@
             <li class="nav-item dropdown">
                 <a class="nav-item nav-link dropdown mr-md-2" href="#" id="bd-versions"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <button class="btn btn-light" type="button"><i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
                             {{ auth()->check() ? auth()->user()->name : 'Account' }}
-                    </button>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
                     @if (!auth()->check())
