e47807ebe02ab907a6ee49868f2e6c21ea765027
Summerborn <devbhai2586@gmail.com>
2021-05-11 14:58:28 +0600

    user details page styled

--- resources/views/front/userdetail/userdetail.blade.php
+++ resources/views/front/userdetail/userdetail.blade.php
@@ -1,4 +1,5 @@
 <link href="{{ asset('/css/userprofileindex.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/product_display.css') }}" rel="stylesheet">
 @extends('front.layouts.master')
 @section('content')
 
@@ -120,75 +121,79 @@
     }
 </style>
 <div class="container flex-row mt-4">
-    <h1 class="border mb-4 heading">Ads by <strong>{{$users->name}}</strong></h1>
+    <h1 class="heading">Ads by <strong>{{$users->name}}</strong></h1>
 <div class="row">
         {{-- For Sale --}}
-        <div class="col-sm offset-1 border border-info sale product-shadow">
-            <h3>For Sale</h3>
-            @foreach($saledata as $i => $sdata)
-                <div class="product-image border-bottom border-top border-success mb-4">
-                        <center>
-                     
-                            <div class="product-info mx-auto d-block border">
-                                <img  height = "200px" src="{{ url('/uploads/') . '/' . $sdata->image }}" alt="Product image here" >
+        <div class = "container">
+            <div class = "products">
+                <h3 class = "lg-title">Products for sale</h3>
+              <div class="row">
+              <!--Products for sale -->
+                @foreach($saledata as $i => $sdata)
+                <div id="saleproduct" class="col-md-3 column">
+                <div class = "product-items">
+                    <!-- single product -->
+                    <div class = "product">
+                        <div class = "product-content">
+                            <div class = "product-img">
+                                <img src="{{ url('/uploads/') . '/' . $sdata->image }}" alt = "product image" height="200px" width="100%">
                             </div>
-                            <div class="border">
-                            <label>Product name <i class="fa fa-product-hunt" aria-hidden="true"></i> :</label>
-                                <h6 class="text-primary">{{$sdata->name}}</h6>
+                        </div>
+          
+                        <div class = "product-info">
+                            <div class = "product-info-top">
+                                <h2 class = "sm-title justify-text-center">{{$sdata->name}}</h2>
                             </div>
-                            <div class="border">
-                            <label>Product category <i class="fa fa-sitemap" aria-hidden="true"></i> :</label>
-                                <h6 class="text-warning">{{$sdata->categories}}</h6>
-                            </div>
-                            <div class="border">
-                            <label>Product price <i class="fa fa-money" aria-hidden="true"></i> :</label>
-                                <h6 class="text-warning">Nu.{{$sdata->price}}</h6>
-                            </div>
-                            <div class="border">
-                            <label>Product description <i class="fa fa-asterisk" aria-hidden="true"></i> :</label>
-                                <h6 class="text-warning">{{$sdata->detail}}</h6>
-                            </div>
-                            <div class="border">
-                            <label>Product location <i class="fa fa-map-marker" aria-hidden="true"></i> :</label>
-                                <h6 class="text-warning">{{$sdata->location}}</h6>
-                            </div>
-                            <span class="text-success h6"> <a href="/front/saledetail/{{$sdata->id}}" class="text-success h6">
-                      View details </a></span><br/>
-                        </center>
+                            <a class = "product-name">Nu. {{$sdata->price}}</a>
+                            <p class = "product-price">{{$sdata->detail}}</p>
+                            <a href="/front/saledetail/{{$sdata->id}}" class="text-success product-name">
+                              View details </a>
+                        </div>
+          
+                        <div class = "off-info">
+                            <h2 class = "sm-title">{{$sdata->negotiation}}</h2>
+                           
+                        </div>
+                        
+                    </div>
+                  </div>
+          
                 </div>
-            @endforeach
-                <!-- {{ $saledata->links() }} -->
-        </div>
+                @endforeach
+                </div>
+              </div>
+            </div>
 
 {{-- For donation --}}
-<div class="col-sm offset-1 border border-info donation product-shadow">
-<h3>For Donation</h3>
-@foreach($dondata as $i => $ddata)
-          <div class="product-image border-bottom border-top border-success mb-4">
-            <center>
-          
-            <div class="product-info mx-auto d-block border">
-                <img  height = "200px" src="{{ url('/uploads/') . '/' . $ddata->image }}" alt="Product image here" >
+<div class ="products" style="margin-top: -30px">
+    <div class ="container">
+      <h3 class="lg-title">Products for donation</h3>
+    <div class="row">
+    @foreach($dondata as $i => $ddata)
+    <div class="col-md-3">
+    <div class = "product-items">
+        <!-- single product -->
+        <div class = "product">
+            <div class = "product-content">
+                <div class = "product-img">
+                    <img src="{{ url('/uploads/') . '/' . $ddata->image }}" alt="Products for donation" height="200px" width="100%">
+                </div>
             </div>
-            <div class="border">
-            <label>Item name <i class="fa fa-product-hunt" aria-hidden="true"></i> :</label>
-                <h6 class="text-primary">{{$ddata->name}}</h6>
-            </div>
-            <div class="border">
-            <label>Product description <i class="fa fa-asterisk" aria-hidden="true"></i> :</label>
-                <h6 class="text-warning">{{$ddata->detail}}</h6>
-            </div>
-            <div class="border">
-            <label>Product location <i class="fa fa-map-marker" aria-hidden="true"></i> :</label>
-                <h6 class="text-warning">{{$ddata->location}}</h6>
-            </div>
-            <span class="text-success h6"> 
-                <a href="/front/donationdetail/{{$ddata->id}}" class="text-success h6">
-                View details </a></span><br/>
-            </center>
-          </div>
-          @endforeach
-          <!-- {{ $dondata->links() }} -->
+
+            <div class = "product-info">
+                <div class = "product-info-top">
+                    <h2 class = "sm-title justify-text-center">Item name: {{$ddata->name}}</h2>
+                </div>
+                <p class = "product-price">Item description: {{$ddata->detail}}</p>
+                <a href="/front/donationdetail/{{$ddata->id}}" class="text-success product-name">
+                  View details </a>
+            </div> 
+        </div>
+      </div>
+    </div>
+      @endforeach  
+</div>
+</div>
 </div>
 </div>
 </div>
