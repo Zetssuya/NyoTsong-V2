c02ebbceb372b18eba58b15b5278dda8b0102320
Summerborn <devbhai2586@gmail.com>
2021-05-13 13:54:42 +0600

    about and contact page added

--- resources/views/front/abtcontact/about.blade.php
+++ resources/views/front/abtcontact/about.blade.php
@@ -1,4 +1,29 @@
+<link href="{{ asset('assets/css/about.css') }}" rel="stylesheet">
 @extends('front.layouts.master')
 @section('content')
+<style>
+    body{
+        background-image: url('/assets/img/about.svg');
+        background-position: right;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 45%;
+    }
+</style>
 
+<div class="aboutus-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="aboutus">
+                    <h2 class="aboutus-title">About NyoTsong</h2>
+                    <p class="aboutus-text">NyoTsong is a progressive web app developed by FInal Year students from IT Department of College of Science and Technology. It is a progressive web app developed for second-hand buying and selling industry in Bhutan.</p>
+                    <p class="aboutus-text"></p>
+                    <a class="aboutus-more" href="#">contact us</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/front/abtcontact/contact.blade.php
+++ resources/views/front/abtcontact/contact.blade.php
@@ -1,4 +1,82 @@
+<link href="{{ asset('assets/css/contact.css') }}" rel="stylesheet">
+<script
+src="https://kit.fontawesome.com/64d58efce2.js"
+crossorigin="anonymous"></script>
 @extends('front.layouts.master')
 @section('content')
+<style>
+    body{
+        background-image: url('/assets/img/contact.svg');
+        background-position: right;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 45%;
+    }
+</style>
+<div id="container">
+    <span class="big-circle"></span>
+    <div class="form">
+      <div class="contact-info">
+        <h3 class="title">Let's get in touch</h3>
+        <p class="text">
+         For any queries related to NyoTsong, do mail uson the below address
+        </p>
 
+        <div class="info">
+          <div class="information">
+            <img src="assets/img/location.png" class="icon" alt="" />
+            <p>College of Science and Technology, Rihchending</p>
+          </div>
+          <div class="information">
+            <img src="assets/img/email.png" class="icon" alt="" />
+            <p>developers@nyotsong.com</p>
+          </div>
+          <div class="information">
+            <img src="assets/img/phone.png" class="icon" alt="" />
+            <p>00975-77773228</p>
+          </div>
+        </div>
+
+        <div class="social-media">
+          <p>Connect with us :</p>
+          <div class="social-icons">
+            <a href="#">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="contact-form">
+        <span class="circle one"></span>
+        <span class="circle two"></span>
+
+        <form action="index.html" autocomplete="off">
+          <h3 class="title">Contact us</h3>
+          <div class="input-container">
+            <input type="text" name="name" class="input" placeholder="username"/>
+            <span>Username</span>
+          </div>
+          <div class="input-container">
+            <input type="email" name="email" class="input" placeholder="email"/>
+            <span>Email</span>
+          </div>
+          <div class="input-container">
+            <input type="tel" name="phone" class="input" placeholder="Phone"/>
+            <span>Phone</span>
+          </div>
+          <div class="input-container textarea">
+            <textarea name="message" class="input" placeholder="Message"></textarea>
+            <span>Message</span>
+          </div>
+          <input type="submit" value="Send Mail" class="btn btn-outline-warning" href="#" />
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <script src="app.js"></script>
 @endsection
