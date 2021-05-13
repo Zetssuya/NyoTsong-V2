47935bb6b74afa74660d19c7caa2a26a9a52a494
tsheringpenjor <tpenjor66.tijtech@gmail.com>
2021-05-14 02:35:15 +0600

    PWA ready

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -9,6 +9,11 @@
     <meta name="author" content="">
     <link rel="icon" href="{{ url('/assets/img/brheader.png')  }}">
     <title>NyoTsong</title>
+<!-- For PWA -->
+<link rel="manifest" href="/manifest.json">
+
+
+
     {{-- for login page --}}
     <link href="{{ asset('/css/userLogin.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -44,11 +49,19 @@
 @include('front.layouts.nav')
     @yield('content')
     
-
+    <script type="text/javascript" >
+window.onload = () => {
+	'use strict';
+  
+	if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker
+			   .register('./sw.js');
+	}
+  }
+</script>
 <!-- Bootstrap core JavaScript -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<!-- <script src="{{url('assets/js/owl.carousel.min.js')}}"></script> -->
 
 @yield('script')
 </body>
